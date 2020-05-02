544b5194783faad827bdc2c179e3a063d1d79f2d
Michal <michal.l.struna@gmail.com>
2020-05-02 16:53:43 +0200

    Add form module.

---
+++ web/src/Form/Constants/FieldType.ts
@@ -0,0 +1,33 @@
+export default {
+
+    TEXT: {
+        name: 'text',
+        validator: () => true
+    },
+
+    NUMBER: {
+        name: 'number',
+        validator: (value: any) => /[0-9]*/.test(value)
+    },
+
+    PASSWORD: {
+        name: 'password',
+        validator: () => true
+    },
+
+    TEXTAREA: {
+        name: 'textarea',
+        validator: () => true
+    },
+
+    DATE: {
+        name: 'date',
+        validator: () => true // TODO: Validate date.
+    },
+
+    SELECT: {
+        name: 'select',
+        validator: () => true
+    }
+
+}
