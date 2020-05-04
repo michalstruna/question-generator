5c88f8fb3078581cb46bd8357f97b8cf07a6077d
Michal <michal.l.struna@gmail.com>
2020-05-04 21:00:29 +0200

    Add swagger.

--- server/src/main/java/cz/michalstruna/questiongenerator/config/AppConfig.java
+++ server/src/main/java/cz/michalstruna/questiongenerator/config/AppConfig.java
@@ -12,4 +12,11 @@ public class AppConfig implements WebMvcConfigurer {
         registry.addMapping("/**");
     }
 
+    @Override
+    public void addResourceHandlers( ResourceHandlerRegistry registry )
+    {
+        registry.addResourceHandler( "swagger-ui.html" ).addResourceLocations( "classpath:/META-INF/resources/" );
+        registry.addResourceHandler( "/webjars/**" ).addResourceLocations( "classpath:/META-INF/resources/webjars/" );
+    }
+
 }
--- server/src/main/java/cz/michalstruna/questiongenerator/controller/TopicController.java
+++ server/src/main/java/cz/michalstruna/questiongenerator/controller/TopicController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/topics")
-@CrossOrigin("*")
 public class TopicController {
 
     @Autowired
