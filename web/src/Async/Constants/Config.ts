13dfab03d433f876205d63ab44c79b731e40f4a2
Michal Struna <michal.l.struna@gmail.com>
2020-05-10 15:01:46 +0200

    Remove ignored files.

--- web/src/Async/Constants/Config.ts
+++ web/src/Async/Constants/Config.ts
@@ -1,4 +1,4 @@
-const isLocalhost = /^https?:\/\/localhost:/.test(window.location.pathname)
+const isLocalhost = window.location.host.includes('localhost:')
 
 export default {
     apiUrl: isLocalhost ? 'http://localhost:8080/api/' : 'https://qgenerator.herokuapp.com/api/'
